4e80ac411fa8c6625476356c9d4bc69e1f76e788
Kannan Thangaraju <kannanoferode@gmail.com>
2019-01-28 07:54:07 +0530

    Associated Python Code

---
+++ PythonCode.py
@@ -0,0 +1,26 @@
+# Import the required module for text 
+# to speech conversion 
+from gtts import gTTS 
+
+# This module is imported so that we can 
+# play the converted audio 
+import os 
+
+# The text that you want to convert to audio 
+mytext = input("Enter the Tamil words in English Language to convert it to audio:")
+
+# Language in which you want to convert 
+language = 'ta'
+
+# Passing the text and language to the engine, 
+# here we have marked slow=False. Which tells 
+# the module that the converted audio should 
+# have a high speed 
+myobj = gTTS(text=mytext, lang=language, slow=False) 
+
+# Saving the converted audio in a mp3 file named 
+# welcome 
+myobj.save("AudioFile.mp3") 
+
+# Playing the converted file 
+os.system("AudioFile.mp3") 
